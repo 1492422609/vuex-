d74aa0caf3595f6f328393e2f8482a3282096dae
SunshineSky <1492422609@qq.com>
2022-09-21 09:35:48 +0800

    初始化vuex项目

---
+++ vue01/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <my-addition></my-addition>
+    <p>------------------</p>
+    <my-subtraction></my-subtraction>
+  </div>
+</template>
+
+<script>
+import AddiTion from "./components/AddiTion";
+import SubtracTion from "./components/SubtracTion";
+export default {
+  components: {
+    "my-addition": AddiTion,
+    "my-subtraction": SubtracTion,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
